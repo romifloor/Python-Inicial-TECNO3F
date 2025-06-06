4b7f3bebafd8eb146362e100df170a42baad71c0
Romina Florencia Ramos <romi859@gmail.com>
2025-06-06 00:18:35 -0300

    Factorial

--- PRÁCTICA 2/trifecta.py
+++ PRÁCTICA 2/trifecta.py
@@ -1,4 +1,11 @@
 
+def calcular_factorial(n):
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    return factorial
+
+
 # Validación de entrada numérica
 while True:
     entrada = input("\nIngrese un número entero (0 para salir): ")
@@ -17,3 +24,18 @@ while True:
     texto = input("Ingrese una palabra o frase: ")
     cantidad_caracteres = len(texto)
     print(f"\nLa palabra/frase tiene {cantidad_caracteres} caracteres.")
+
+    # Factorial    
+    if cantidad_caracteres < 0:
+        print("No se puede calcular factorial de números negativos.")
+    else:
+        # Cálculo del factorial
+        factorial = calcular_factorial(cantidad_caracteres)
+        print(f"El factorial de {cantidad_caracteres} es: {factorial}")
+        
+        if factorial % 2 == 0:
+            print("El resultado es PAR.")
+        else:
+            print("El resultado es IMPAR.")
+    
+    print("\n" + "="*50)  
